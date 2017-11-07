9fb31dc8782749c071a1deac6442162f13c45cf9
Mohammad Khan <sparrowangels@gmail.com>
2017-11-07 06:55:27 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,43 +1,67 @@
-require 'pry'
-
 class String
-  # Define this instance method to return true if the string you are calling it on ends in a period
-  # and false if it does not.
   def sentence?
-    if self.end_with?(".")
-      return true
-    end
-    false
+    self.end_with?(".")
   end
-
   def question?
-    if self.end_with?("?")
-      return true
-    end
-    false
+    self.end_with?("?")
   end
-
   def exclamation?
-    self.end_with?("!") ? true : false
+    self.end_with?("!")
   end
-
   def count_sentences
-    # puts "******#{self}****"
-    puts self.split(/[.?!]/).inspect
-    sarray = self.split(/[.?!]/)
-    blank = 0
-    sarray.each do |sentence|
-      if sentence.length == 0
-        blank +=1
-      end
-    end #sarray.each
-    puts "sarray #{sarray}"
+    # puts self
+    # puts "*************************************************#{self.split(/[.!?]/)}"
 
-    self.split(/[.?!]/).length-blank
+    # self.split(/[.!?]/).length
+    self.split(/[.!?]/).reject{|s| s.empty?}.length
+    # noempty =     self.split(/[.!?]/).reject {|s| s.empty?}
+    # noempty.length
   end
 end
+#monkey patching
 
-# puts "one. two. three?"
-# puts "one. two. three?".count_sentences
-puts "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
-puts "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
+
+# require 'pry'
+#
+# class String
+#   # Define this instance method to return true if the string you are calling it on ends in a period
+#   # and false if it does not.
+#   def sentence?
+#     if self.end_with?(".")
+#       return true
+#     end
+#     false
+#   end
+#
+#   def question?
+#     if self.end_with?("?")
+#       return true
+#     end
+#     false
+#   end
+#
+#   def exclamation?
+#     self.end_with?("!") ? true : false
+#   end
+#
+#   def count_sentences
+#     # puts "******#{self}****"
+#     puts self.split(/[.?!]/).inspect
+#     sarray = self.split(/[.?!]/)
+#     blank = 0
+#     sarray.each do |sentence|
+#       if sentence.length == 0
+#         blank +=1
+#       end
+#     end #sarray.each
+#     puts "sarray #{sarray}"
+#
+#     self.split(/[.?!]/).length-blank
+#   end
+# end
+#
+# # puts "one. two. three?"
+# # puts "one. two. three?".count_sentences
+# puts "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+# puts "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
+# ences
