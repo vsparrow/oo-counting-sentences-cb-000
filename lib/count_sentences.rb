af093abc288fb0b8f62e7c182081384fb40ff403
vsparrow <khan@vsparrow.com>
2019-02-13 00:44:23 -0500

    completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    s = self.dup.split(" ")
+    count = 0
+    s.each do |w|
+      if w.sentence? || w.question? || w.exclamation?
+        count += 1
+      end
+    end
+    return count
   end
 end
